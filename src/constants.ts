b2f9475dcd9c58d8b3ecf561c2a055f1579ddd05
admin7654321 <admin7654321@example.com>
2026-09-07 01:44:37 +0300

    Admin Dedicated OTA Update 1.0.245

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { Capacitor } from '@capacitor/core';
 import { CapacitorUpdater } from '@capgo/capacitor-updater';
 import { Preferences } from '@capacitor/preferences';
 
-export const OTA_VERSION = '1.0.244';
+export const OTA_VERSION = '1.0.245';
 export const APK_VERSION = '1.0.71';
 export const CLOUDFLARE_AUTH_URL = 'https://entersave-auth.admin-a.workers.dev';
 
